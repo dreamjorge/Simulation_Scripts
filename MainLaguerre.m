578998ecfcc40712a3cad1f4fb80f64e404266ca
jorgeugalde <jorge-kun@live.com>
2020-01-07 22:28:56 -0600

    minor changes

--- MainLaguerre.m
+++ MainLaguerre.m
@@ -5,8 +5,8 @@ addpath ParaxialBeams\Addons
 mapgreen = AdvancedColormap('kgg',256,[0 100 255]/255);  %color of beam
 
 %-------------------- indices of Laguerre Gaussian Beams -----------------%
-nu      = 19;
-mu      = 0;
+nu = 19;
+mu = 0;
 
 % Calculating Laguerre Parameters in z = 0
 
@@ -18,7 +18,8 @@ Wavelength           = 0.6328;
 % InitialWaist         = 1;
 % Wavelength           = pi;
 % Estimating parameter of laguerre in z=0
-LP                   = LaguerreParameters(0,InitialWaist,Wavelength,nu,mu);
+PropagationDistance  = 0;
+LP                   = LaguerreParameters(PropagationDistance,InitialWaist,Wavelength,nu,mu);
 
 
 % Initial Waist of Laguerre Beam
@@ -28,7 +29,7 @@ LaguerreInitialWaist = LP.LaguerreWaist;
 %First we estimate sampling in z-direction with propagation distance 
 % z-direction
 Dz = LP.RayleighDistance;    % z-window (propagation distance)
-Nz = 2^6;                    % number of points in z-direction
+Nz = 2^5;                    % number of points in z-direction
 dz = Dz/Nz;                  % Resolution in z
 z  = 0:dz:Dz;                % z-vector z of propagation 
 
@@ -37,7 +38,7 @@ z  = 0:dz:Dz;                % z-vector z of propagation
 
 MaxLaguerreWaist = LaguerreParameters.waistFunction(z(end),InitialWaist,LP.RayleighDistance,nu,mu);
 
-N   = 2^10;                  % Number of points in x,y axis
+N   = 2^8;                  % Number of points in x,y axis
 n   = -N/2+.05:N/2-1+.05;    % vector with N-points with resolution 1
 Dx  = 2.2*MaxLaguerreWaist;  % Size of window 
 dx  = Dx/N;                  % Resolution
@@ -57,7 +58,8 @@ kx   = 2*pi*u;
 [Kx] = meshgrid(kx);
 
 %% ----------------------- Laguerre Gauss in z = 0 --------------------- %%
-LG  = LaguerreBeam(X,X',0,InitialWaist,Wavelength,nu,mu);
+PropagationDistance  = 0;
+LG                   = LaguerreBeam(X,X',PropagationDistance,InitialWaist,Wavelength,nu,mu);
 
 % Plot of Field
 figure(1)
@@ -75,14 +77,16 @@ g   = LG.OpticalField;
 pxy = max(max(g));
 
 %% ----------------- Obstruction on Lagurre in z = 0 ------------------- %%
-lo      = LaguerreParameters.waistFunction(0,InitialWaist,LP.RayleighDistance,nu,mu)/4.3; % size of obstruction in terms of waist of Laguerre
-xt      = 0;                                                                              % traslation of obstruction in x-axis
-yt      = 0;                                                                              % traslation of onstruction in y-axis
-[~,rho] = cart2pol(X-xt,X'-yt);                                                           % Convert this in polar coordinates
-obo     = double(rho<=lo);                                                                % Create Obstruction   
-clear rho                                                                                 % Clean Matrix of polar coordinates
+PropagationDistance = 0;
+lo                  = (LaguerreParameters.waistFunction(...
+                       PropagationDistance,InitialWaist,LP.RayleighDistance,nu,mu))/4.3; % size of obstruction in terms of waist of Laguerre
+xt                  = 0;                                                                              % traslation of obstruction in x-axis
+yt                  = 0;                                                                              % traslation of onstruction in y-axis
+[~,rho]             = cart2pol(X-xt,X'-yt);                                                           % Convert this in polar coordinates
+obo                 = double(rho<=lo);                                                                % Create Obstruction   
+clear rho                                                                                             % Clean Matrix of polar coordinates
 % Applying obstruction in optic field
-g       = g.*(1-obo);
+g                   = g.*(1-obo);
 %Ploting Laguerre with obstruction
 figure(20)
 pcolor(x,x,abs(g).^2)
@@ -97,14 +101,26 @@ ylabel('$y$','Interpreter','latex','FontSize',18)
 
 %% ----------------------- Ray tracing (rx,z=0)  ----------------------- %%
 
-pn        =  8;          % Number of rays
-rayH1(pn) = OpticalRay;
-rayH2(pn) = OpticalRay;
+pn          =  8;          % Number of rays
+rayH1(pn)   = OpticalRay;
+rayH2(pn)   = OpticalRay;
+% temp th for estimate cross in origin 
+thTempPrev  = zeros(1,pn);
+thTemp      = zeros(1,pn);
+rObst       = zeros(1,pn);
+rAcum       = zeros(1,pn);
+rTempPrev   = zeros(1,pn);
+rTempActual = zeros(1,pn);
+
 
 for jj = 1:pn
     % Cartersian coordinates of point in circunference of obstruction
     xi = xt+lo*cos(jj*(2*pi)/(pn)); 
     yi = yt+lo*sin(jj*(2*pi)/(pn));
+    %estimate radii of point (x,y) to origin
+    rObst(jj)     = sqrt(xi^2+yi^2);
+    %for iterative process in propagation 
+    rTempPrev(jj) = rObst(jj);
     
     % saving cartesian coordinates of point in circunference in each ray
     rayH1(jj).xCoordinate(1) = xi; 
@@ -112,15 +128,16 @@ for jj = 1:pn
     rayH2(jj).xCoordinate(1) = xi; 
     rayH2(jj).yCoordinate(1) = yi;
     
+    HankelType  = 1;
     [rayH1(jj)] = HankelLaguerre.getLaguerreSlopes(rayH1(jj),x,y,z,...
                                                    dx,dx,dz,...
                                                    xi,yi,0,...
-                                                   InitialWaist,Wavelength,nu,mu,1);
-    
+                                                   InitialWaist,Wavelength,nu,mu,HankelType);
+    HankelType  = 2;
     [rayH2(jj)] = HankelLaguerre.getLaguerreSlopes(rayH1(jj),x,y,z,...
                                                    dx,dx,dz,...
                                                    xi,yi,0,...
-                                                   InitialWaist,Wavelength,nu,mu,2);
+                                                   InitialWaist,Wavelength,nu,mu,HankelType);
 end
 
 % Initial Field with rays in this init conditions
@@ -143,8 +160,8 @@ hold off
 
 %%  ----------------------- Physical Propagation ------------------------ %
 % paraxial propagator 
-k    = LP.k;
-prop = exp(1i*dz*(Kx.^2+(Kx').^2)/(2*k));
+k       = LP.k;
+prop    = exp(1i*dz*(Kx.^2+(Kx').^2)/(2*k));
 figure(5)
 imagesc(u,u,(angle(prop)))
 title('Propagator')
@@ -187,7 +204,7 @@ for ii = 2:length(z) % propagation with respect to z
     % Given z we find z+dz and new position x+dx and y+dy with help of
     % slopes
 
-    for jj=1:pn
+    for jj = 1:pn
         % step of ray in z-direction, equation or ray r(z) = mrz*dz+r(z-1)
         % dependece of last point, new vector
         rayH1(jj).xCoordinate(ii) = rayH1(jj).xCoordinate(ii-1) + (1/rayH1(jj).zxSlope)*dz;
@@ -198,22 +215,46 @@ for ii = 2:length(z) % propagation with respect to z
   
         % ------------------ Calculating Slopes ------------------------- %
         % point for H1
-        xi = rayH1(jj).xCoordinate(ii);
-        yi = rayH1(jj).yCoordinate(ii);
-        zi = z(ii);
-
+        xi          = rayH1(jj).xCoordinate(ii);
+        yi          = rayH1(jj).yCoordinate(ii);
+        zi          = z(ii);
+        HankelType  = 1;
+        
         [rayH1(jj)] = HankelLaguerre.getLaguerreSlopes(rayH1(jj),x,y,z,...
                                                        dx,dx,dz,...
                                                        xi,yi,zi,...
-                                                       InitialWaist,Wavelength,nu,mu,1);
-        % point of H2 
-        xi = rayH2(jj).xCoordinate(ii);
-        yi = rayH2(jj).yCoordinate(ii);
-        zi = z(ii);                                    
-        [rayH2(jj)] = HankelLaguerre.getLaguerreSlopes(rayH2(jj),x,y,z,...
-                                                       dx,dx,dz,...
-                                                       xi,yi,zi,...
-                                                       InitialWaist,Wavelength,nu,mu,2);
+                                                       InitialWaist,Wavelength,nu,mu,HankelType);
+
+        % point of H2
+        
+        xi          = rayH2(jj).xCoordinate(ii);
+        yi          = rayH2(jj).yCoordinate(ii);
+        zi          = z(ii); 
+
+
+        %----------condition for cross around origin
+        rTempActual(jj) = sqrt(xi^2+yi^2);
+        drTemp          = abs( rTempPrev(jj) - rTempActual(jj) );
+        rAcum(jj)       = (rAcum(jj) + drTemp);
+        
+        if ( rAcum(jj)<rObst(jj) )
+          
+          HankelType  = 2;
+          [rayH2(jj)] = HankelLaguerre.getLaguerreSlopes(rayH2(jj),x,y,z,...
+                                                         dx,dx,dz,...
+                                                         xi,yi,zi,...
+                                                         InitialWaist,Wavelength,nu,mu,HankelType);
+        else
+           
+          HankelType  = 1;
+          [rayH2(jj)] = HankelLaguerre.getLaguerreSlopes(rayH2(jj),x,y,z,...
+                                                         dx,dx,dz,...
+                                                         xi,yi,zi,...
+                                                         InitialWaist,Wavelength,nu,mu,HankelType);                                    
+        end
+        
+        rTempPrev(jj) = rTempActual(jj);
+                                                     
     end
     %------------------------ End calculating rays -----------------------%   
     %propagating field
@@ -225,18 +266,18 @@ for ii = 2:length(z) % propagation with respect to z
     imagesc(angle(G))
     %G = G.*exp(1i*pi*50);
     figure(8)
-    imagesc(angle(G))
+    imagesc(angle(g))
     %obtain new propagated field
     %saving transversal fields
-    gx(:,ii)=g(N/2+1,:);
-    gy(:,ii)=g(:,N/2+1);
+    gx(:,ii) = g(N/2+1,:);
+    gy(:,ii) = g(:,N/2+1);
     
     figure(9)
-    H1=XLaguerreBeam(X,X',zi,InitialWaist,Wavelength,nu,mu);
+    H1 = XLaguerreBeam(X,X',zi,InitialWaist,Wavelength,nu,mu);
     imagesc(angle(H1.OpticalField))
     
     figure(10)
-    H2=HankelLaguerre(X,X',zi,InitialWaist,Wavelength,nu,mu,2);
+    H2 = HankelLaguerre(X,X',zi,InitialWaist,Wavelength,nu,mu,2);
     imagesc(angle(H2.OpticalField))
 %     
 %     
