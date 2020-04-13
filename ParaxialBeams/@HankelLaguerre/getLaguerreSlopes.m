f5ba7fc637a140e447ca6700b9a200084c2d3d8e
jorgeugalde <jorge-kun@live.com>
2020-04-12 20:04:39 -0500

    changing to cartesians in laguerre
    Related: CylindricalCoordinates

--- ParaxialBeams/@HankelLaguerre/getLaguerreSlopes.m
+++ ParaxialBeams/@HankelLaguerre/getLaguerreSlopes.m
@@ -1,6 +1,6 @@
-function [rayObject] = getLaguerreSlopes(rayObject,r,th,z,...
-                                         dr,dth,dz,...
-                                         ri,thi,zi,...
+function [rayObject] = getLaguerreSlopes(rayObject,...
+                                         r,th,z,...
+                                         dr,...
                                          InitialWaist,Wavelength,nu,mu,numberHankel)
                                        
 % This function estimates components (x,y,z) of a ray object and its slopes
@@ -14,25 +14,48 @@ function [rayObject] = getLaguerreSlopes(rayObject,r,th,z,...
 %                                           dx,dy,dz,...
 %                                           xi,yi,zi,...
 %                                           InitialWaist,Wavelength,nu,mu,numberHankel)
-                                                 
-  %th = cte, z = cte
-  HLr = HankelLaguerre(r,thi,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+       
+
+  ri   = rayObject.rCoordinate;
+  thi  = rayObject.thetaCoordinate;
+  zi   = rayObject.zCoordinate;
   
-  %x = cte, z = cte
-  HLth = HankelLaguerre(ri,th,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+  %transform coordinates to cartsians
+  [xi,yi] = pol2cart(thi,ri);
+  [x,y]   = pol2cart(th,r);
+  
+  %vriable
+  
+  HLx     = HankelLaguerre(sqrt(x.^2 + yi.^2),atan2(yi,x) ,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+  HLy     = HankelLaguerre(sqrt(xi.^2+ y.^2) ,atan2(y,xi) ,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+  HLz     = HankelLaguerre(sqrt(xi.^2+ yi.^2),atan2(yi,xi),z ,InitialWaist,Wavelength,nu,mu,numberHankel);
+  
+  fx      = unwrap(angle(HLx.OpticalField));
+  fy      = unwrap(angle(HLy.OpticalField));
+  fz      = unwrap(angle(HLz.OpticalField));
 
-  %x = cte, y = cte
-  HLz = HankelLaguerre(ri,thi,z,InitialWaist,Wavelength,nu,mu,numberHankel);
-
-  fr   = unwrap(angle(HLr.OpticalField));
-  fth  = unwrap(angle(HLth.OpticalField));
-  fz   = unwrap(angle(HLz.OpticalField));
-
-  %Estimate k with wavelength
   k   = (2*pi)/Wavelength;
   
-  %Calculating gradient for slopes of vector normal
-  [rayObject.zrSlope, rayObject.zthSlope, rayObject.rthSlope] = ...
-   gradientCylindrical(fr,fth,fz,k,dr,dth,dz,ri,thi,zi);
+  [rayObject] = gradientxyz(fx,fy,fz,k,dr,rayObject);
+  
+%   %th = cte, z = cte
+%   HLr  = HankelLaguerre(r,thi,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+%   
+%   %r = cte, z = cte
+%   HLth = HankelLaguerre(ri,th,zi,InitialWaist,Wavelength,nu,mu,numberHankel);
+% 
+%   %r = cte, th = cte
+%   HLz  = HankelLaguerre(ri,thi,z,InitialWaist,Wavelength,nu,mu,numberHankel);
+% 
+%   fr   = unwrap(angle(HLr.OpticalField));
+%   fth  = unwrap(angle(HLth.OpticalField));
+%   fz   = unwrap(angle(HLz.OpticalField));
+% 
+%   %Estimate k with wavelength
+%   k   = (2*pi)/Wavelength;
+  
+%   %Calculating gradient for slopes of vector normal
+%   [rayObject] = ...
+%    gradientCylindrical(fr,fth,fz,k,dr,rayObject);
 
 end
--- ParaxialBeams/Addons/gradientCylindrical.m
+++ ParaxialBeams/Addons/gradientCylindrical.m
@@ -1,15 +1,24 @@
-function [mzr,mztheta,mrtheta] = gradientCylindrical(fr,ftheta,fz,k,dr,dtheta,dz,r,theta,z) 
+function [rayObject] = gradientCylindrical(fr,ftheta,fz,k,...
+                                           difr,...
+                                           rayObject) 
 
-  %partial derivatives
+  r      =  rayObject.rCoordinate;
+  theta  =  rayObject.thetaCoordinate;
+  z      =  rayObject.zCoordinate;
   
+  tempdr         = num2cell(difr);
+  [dr,dtheta,dz] = deal(tempdr{:});
+
+
+  %partial derivatives 
   gr     = gradient(fr)/dr;
   gtheta = (1./r).*(gradient(ftheta)/dtheta);
   gz     = gradient(fz)/dz+k;
   N      = size(gr,2);
 
-  %
-  mzr     = gz(floor(z/dz+1))    /gr(N/2+1+floor(r/dr));
-  mztheta = gz(floor(z/dz+1))    /gtheta(N/2+1+floor(theta/dtheta));
-  mrtheta = gr(N/2+1+floor(r/dr))/gtheta(N/2+1+floor(theta/dtheta));
+  rayObject.zrSlope  = gz(floor(z/dz+1))    /gr(N/2+1+floor(r/dr));
+  rayObject.zthSlope = gz(floor(z/dz+1))    /gtheta(N/2+1+floor(theta/dtheta));
+  rayObject.rthSlope = gr(N/2+1+floor(r/dr))/gtheta(N/2+1+floor(theta/dtheta));
+  
 
 end
