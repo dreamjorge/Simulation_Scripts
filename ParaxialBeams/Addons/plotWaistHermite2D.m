bb93f797153816a050fedd08acd347afec971fa9
Ugalde <uidn7961@contiwan.com>
2020-10-05 16:21:03 -0500

    -Clean code in MainHermiteThesis Script. -Added documentation on getWaistOneDirection. -Added documentation on plot square. -Added plotWaistHermite2D for plot waist of Hermite.

---
+++ ParaxialBeams/Addons/plotWaistHermite2D.m
@@ -0,0 +1,21 @@
+function [h] = plotWaistHermite2D(HermiteParametersZi,scale,color)
+%% Function for plot waist of Hermite Gauss Beam in 2D at z - distance given in parameters
+% Recives Hermite Parameters and returns plot object.
+  
+  % Parameters needed from Hermite Parameters
+  rd     = HermiteParametersZi.RayleighDistance;
+  wo     = HermiteParametersZi.InitialWaist;
+  n      = HermiteParametersZi.n;
+  m      = HermiteParametersZi.m;
+  zi     = HermiteParametersZi.zCoordinate;
+  % Waist 1-dimentional in each axes
+  wx     = HermiteParametersZi.getWaistOneDirection(zi,wo,rd,n);
+  wy     = HermiteParametersZi.getWaistOneDirection(zi,wo,rd,m);
+  % Vertices of square
+  xwaist = [-wx/2,  wx/2, wx/2, -wx/2];
+  ywaist = [-wy/2, -wy/2, wy/2,  wy/2];
+  % Plot Waist of Hermite Gauss Beam
+  h      = plotSquare(xwaist/scale,ywaist/scale,color);
+
+
+end
