9e381939cd455c48ced7d80aa323f36da9ad6c2b
jorge ugalde <jorge-kun@live.com>
2020-10-05 16:21:03 -0500

    -Clean code in MainHermiteThesis Script. -Added documentation on getWaistOneDirection. -Added documentation on plot square. -Added plotWaistHermite2D for plot waist of Hermite.

--- ParaxialBeams/@HermiteParameters/getWaistOneDirection.m
+++ ParaxialBeams/@HermiteParameters/getWaistOneDirection.m
@@ -1,6 +1,12 @@
-function waistH = getWaistOnedirection(zDistance,InitialWaist,RayleighDistance,l)
+function waistH = getWaistOneDirection(zDistance,InitialWaist,RayleighDistance,n)
+%% Function for Obtain of waist of Hermite Guassian in 1-D
+% Recives next elements
+% - zDistance         Distance in z for estimate waist.
+% - InitialWaist      Waist of Gaussian Beam at 0 distance.
+% - RayleighDistance  Distance of Rayleigh of Guassian Beam.
+% - n                 Number of Poylinolial Hermite.
 
-  waistH = sqrt(2*l+1)...
+  waistH = sqrt(2)*sqrt(2*n+1)...
          .*(InitialWaist).*sqrt((zDistance./RayleighDistance).^2+1); % factor of Gaussian waist
 
 end
