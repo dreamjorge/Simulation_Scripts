55232b51d6da2ab5f82a691851211f20bf01ac9d
JorgeUgalde <jorge-kun@live.com>
2020-10-11 23:34:22 -0500

    Allign code

--- ParaxialBeams/Addons/assignCoordinates2CylindricalRay.m
+++ ParaxialBeams/Addons/assignCoordinates2CylindricalRay.m
@@ -1,17 +1,18 @@
 function    [Ray] = assignCoordinates2CylindricalRay(xi,yi,zi,Ray,point_index,hankelType)
+%%Assign coordinates of ray in (xi,yi,zi) point, calculate cylindrical
+%%coordinates and put this information on ray structure.
 
   [thi,ri]  = cart2pol(xi,yi);
 
-
-  Ray.rCoordinate(point_index)       =   ri;
-  Ray.thetaCoordinate(point_index)   =  thi;
-
-  Ray.xCoordinate(point_index) = xi;
-  Ray.yCoordinate(point_index) = yi;
-  Ray.zCoordinate(point_index) = zi;
+  % Save coordinates in structure
+  Ray.rCoordinate    (point_index) = ri;
+  Ray.thetaCoordinate(point_index) = thi;
+  Ray.xCoordinate    (point_index) = xi;
+  Ray.yCoordinate    (point_index) = yi;
+  Ray.zCoordinate    (point_index) = zi;
   
   %initial conditions of slopes
-  Ray.zrSlope(point_index)  = Inf;
+  Ray.zrSlope (point_index) = Inf;
   Ray.zthSlope(point_index) = Inf;
   Ray.rthSlope(point_index) = Inf;
   
