9295dda5c296c96cf9e14a1265293a1e5bb61d22
jorge ugalde <jorge-kun@live.com>
2020-10-12 20:55:43 -0500

    Added elegant Laguerre Beam Class

---
+++ ParaxialBeams/@eLaguerreBeam/eLaguerreBeam.m
@@ -0,0 +1,82 @@
+classdef eLaguerreBeam <  matlab.mixin.Copyable & handle & LaguerreParameters & GaussianBeam
+  %% Laguerre Gaussian Beam is a scalar optical field with its parameters defined in
+  %properties.
+  
+  % Example:
+  % LB = LaguerreBeam(rCoordinate,thetaCoordinate,LaguerreParameters);
+  % where PropagationDistance,rCoordinate,rCoordinate can be scalar, vector or matrix.  
+
+  properties (Dependent)
+    %% Properties dependient of Laguerre Gauss Beam
+    LaguerreAmplitude     % Laguerre Gauss factor for Amplitude
+    OpticalFieldLaguerre  % Optical Field of Laguerre Gauss
+  end
+  
+  properties
+    %% Properties independent of Laguerre Gauss Beam
+    thetaCoordinate       % Azimutal coordinate in Cylindrical Coordinates
+    Normalization         % Normalization Factor 
+  end
+  
+  methods(Static)
+    %% Formula for Obtain Associated Laguerre Polynomial (Lnm)
+    Lnm     = AssociatedLaguerrePolynomial(nu,mu,x);
+  end
+  
+  methods
+    
+    function LaguerreAmplitude = get.LaguerreAmplitude(obj)
+      %% Factor of Amplitude for Laguerre Gauss Beam
+      LaguerreAmplitude = 1;...(1./obj.Waist).*((sqrt(2)*(obj.RadialCoordinate))./obj.Waist).^abs(obj.p);%obj.l);
+    end
+
+    function Normalization = get.Normalization(obj)
+     %% Factor of Normalizartion for Laguerre Gauss Beam
+      Normalization =  sqrt(2*factorial(obj.p)/(pi*factorial(obj.p+abs(obj.l))));
+    end
+
+    function Laguerre = eLaguerreBeam(rCoordinate,thetaCoordinate,LaguerreParameters)
+      %% Constructor of Laguerre Gauss Beam
+      
+      % Copying LaguerreParameters to LaguerreBeam Object
+      Laguerre@LaguerreParameters(LaguerreParameters.zCoordinate...
+                                 ,LaguerreParameters.InitialWaist...
+                                 ,LaguerreParameters.Wavelength...
+                                 ,LaguerreParameters.l...
+                                 ,LaguerreParameters.p);
+      
+      %Copying Gaussian Beam to LaguerreBeam Object
+      Laguerre@GaussianBeam(rCoordinate,LaguerreParameters); 
+      % Copying input to properties 
+      Laguerre.thetaCoordinate  = thetaCoordinate;
+
+    end
+    
+    function opticalField =get.OpticalFieldLaguerre(obj)
+      %% Calculating Optical Field of Laguerre
+      PhiPhase     = obj.PhiPhase;
+      l            = obj.l;
+      p            = obj.p;
+      theta        = obj.thetaCoordinate;
+      r            = obj.rCoordinate;
+%       xArgument    = (2*(r.^2))./(waist.^2);
+      
+      q            = obj.zCoordinate+1i*obj.RayleighDistance;
+      k            = obj.k;
+      alpha        = 1i*(k)./(2*q); 
+      
+      opticalField = ...Laguerre.Normalization.*...
+                     ...Laguerre.LaguerreAmplitude.*... 
+                     exp( 1i*PhiPhase).*...
+                     exp(-1i*abs(p)*(theta)).*...
+                     LaguerreBeam.AssociatedLaguerrePolynomial(l,abs(p),alpha.*r.^2).*...
+                     obj.OpticalField;
+    end
+    
+    function [] =set.OpticalFieldLaguerre(~,~)
+      %% Block OpticalFieldLaguerre as input in Lagurre Gauss Beam
+       error('You cannot set OpticalField property'); 
+    end
+  end
+  
+end
