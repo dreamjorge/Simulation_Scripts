5384834ca71a6800a458b15f98713d44d7a24517
Ugalde <uidn7961@contiwan.com>
2020-10-12 20:55:07 -0500

    This works witth removing origin r==0

--- ParaxialBeams/@HankelLaguerre/getPropagateCylindricalRays.m
+++ ParaxialBeams/@HankelLaguerre/getPropagateCylindricalRays.m
@@ -16,31 +16,37 @@ function [Rays] = getPropagateCylindricalRays(Rays,...
   temporalRay = CylindricalRay(); 
 
   for point_index = 1 : TotalRays
+    
     % copy data a temporal ray 
     temporalRay = copyArrayRay2Ray(Rays,temporalRay,point_index);
-
-    %% obtain new coordinates of ray
-    temporalRay.rCoordinate     = temporalRay.rCoordinate     + (1./temporalRay.zrSlope )*dz;
-    temporalRay.thetaCoordinate = temporalRay.thetaCoordinate + (1./temporalRay.zthSlope)*dz;
+    % slopes
+    zrSlope                     = temporalRay.zrSlope;
+    zthSlope                    = temporalRay.zthSlope;
+    % propagate ray 
+    temporalRay.rCoordinate     = temporalRay.rCoordinate     + (1./zrSlope )*dz;
+    temporalRay.thetaCoordinate = temporalRay.thetaCoordinate + (1./zthSlope)*dz;
     temporalRay.zCoordinate     = temporalRay.zCoordinate     + dz;
-    % obtain new (x,y) coordinates 
-    [Rays.xCoordinate,Rays.yCoordinate] = pol2cart(Rays.thetaCoordinate,Rays.rCoordinate);
-
+   
     % taking this new coordinates of point to calculate hankels
     ri  = temporalRay.rCoordinate;
     thi = temporalRay.thetaCoordinate;
 
-    
+    % Condition for cross origin (Change hankel type)
     if (ri < 0) && (HankelType == 2)
       temporalRay.hankelType = 1;
+      [xi,yi] = pol2cart(thi,ri);
+      temporalRay.xCoordinate = xi;
+      temporalRay.yCoordinate = yi;
+      ri      = abs(ri);
     end 
     
-    % always it takes positive value of r
-    ri                      = abs(ri);
+    
     temporalRay.rCoordinate = ri;
     
+     % obtain new (x,y) coordinates 
+    [temporalRay.xCoordinate,temporalRay.yCoordinate] = pol2cart(thi,ri);
+
     % calculating Hankels in point (r,th,z) of ray
-    
     HLr  = HankelLaguerre(r ,thi,LParametersZi,temporalRay.hankelType);
     HLth = HankelLaguerre(ri,th ,LParametersZi,temporalRay.hankelType);
     HLz  = HankelLaguerre(ri,thi,LParametersZ ,temporalRay.hankelType);     
@@ -49,7 +55,7 @@ function [Rays] = getPropagateCylindricalRays(Rays,...
     fth  = unwrap(angle(HLth.OpticalFieldLaguerre));
     fz   = unwrap(angle( HLz.OpticalFieldLaguerre));
 
-     % Calculating gradient
+     % Calculating gradient (avoid introduce r == 0 coordinate)
     [temporalRay] = getCylindricalGradient(fr,fth,fz,k,difr,temporalRay);
     
     % copying new coordinates of ray to object that includes all rays
