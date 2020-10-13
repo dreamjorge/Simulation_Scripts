e18ae90fb149243471b852c1d8fae8f046523fb2
jorge ugalde <jorge-kun@live.com>
2020-10-12 20:54:37 -0500

    remove extra lines of code

--- ParaxialBeams/Addons/getCylindricalGradient.m
+++ ParaxialBeams/Addons/getCylindricalGradient.m
@@ -1,4 +1,7 @@
-function [rayObject] = getCylindricalGradient(fr,ftheta,fz,k,...
+function [rayObject] = getCylindricalGradient(fr,...
+                                              ftheta,...
+                                              fz,...
+                                              k,...
                                               difr,...
                                               rayObject) 
 %% Function for calculate gradient in (x,y,z) coordinates in rayObject
@@ -10,7 +13,6 @@ function [rayObject] = getCylindricalGradient(fr,ftheta,fz,k,...
   tempdr         = num2cell(difr);
   [dr,dtheta,dz] = deal(tempdr{:});
 
-
   %partial derivatives 
   gr     = gradient(fr)/dr;
   gtheta = (1./r).*(gradient(ftheta)/dtheta);
@@ -20,24 +22,6 @@ function [rayObject] = getCylindricalGradient(fr,ftheta,fz,k,...
   rayObject.zrSlope  = gz(floor(z/dz+1))/gr(floor(r/dr)+1);
   rayObject.zthSlope = gz(floor(z/dz+1))/gtheta(N/2+1+floor(theta/dtheta));
   rayObject.rthSlope = gr(floor(r/dr+1))/gtheta(N/2+1+floor(theta/dtheta));
-  
-  if isnan(rayObject.zrSlope)
-  
-    rayObject.zrSlope = Inf;
-    
-  end
-  
-  if isnan(rayObject.zthSlope)
-  
-    rayObject.zthSlope = Inf;
-    
-  end
-  
-    
-  if isnan(rayObject.rthSlope)
-  
-    rayObject.rthSlope = Inf;
-    
-  end
+
 
 end
