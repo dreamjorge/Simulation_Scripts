7b4dedc8c766dbf4705309204cf8b580613cddd8
Ugalde <uidn7961@contiwan.com>
2020-10-14 20:01:27 -0500

    added x,y scale input

--- ParaxialBeams/Addons/Plots_Functions/plotRays.m
+++ ParaxialBeams/Addons/Plots_Functions/plotRays.m
@@ -1,9 +1,9 @@
-function [] = plotRays(rayObject,color,scale)
+function [h] = plotRays(rayObject,color,scaleX,scaleY)
+%% Function for plot rays in x,y plane
+  hold on
 
-hold on
+  h = scatter(rayObject.xCoordinate*scaleX,rayObject.yCoordinate*scaleY,10,color,'filled','o');
 
-scatter(rayObject.xCoordinate/scale,rayObject.yCoordinate/scale,10,color,'filled','o')
-pause(0.05)
-hold off
+  hold off
 
 end
