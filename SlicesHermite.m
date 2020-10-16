6f2d674804221dbd04233b55de6b7ca4ecb01043
JorgeUgalde <jorge-kun@live.com>
2020-10-16 18:56:23 -0500

    Modification for Intensity

--- SlicesHermite.m
+++ SlicesHermite.m
@@ -10,14 +10,15 @@ ymax  = max(xnorm(:));
 zmin  = min(xnorm(:));  
 zmax  = max(xnorm(:));
 
-% Introduce Nan Values on Marix
+% Introduce Nan Values on Marixf
 bb         = Wo;
-iindex     = find(abs(bb)<=.0005);
+iindex     = find(abs(bb)<=.000007);
 bb(iindex) = NaN;
 
 
 close(figure(101))
-figure (101)
+fig = figure (101);
+fig.Position = [1 31 1920 973];
 plotPropagatedRays(rayH11,rayH12,rayH21,rayH22,1,1)
 hold on 
 daspect([1.5,.1,.1]) 
@@ -30,7 +31,7 @@ axis off
 aa = permute(bb,[3 2 1]);
 %distances of propagation
 zprop  = [0, 6*Dz/3 , Dz/2, 0.99*Dz];
-hzprop = slice(znorm,ynorm,xnorm,abs(aa ),zprop,[],[]); 
+hzprop = slice(znorm,ynorm,xnorm,abs(aa).^2,zprop,[],[]); 
 hzprop(1).FaceColor = 'interp'; 
 hzprop(2).FaceColor = 'interp';
 hzprop(3).FaceColor = 'interp';
@@ -42,3 +43,42 @@ hzprop(4).EdgeColor = 'none';
 colormap(mapgreen)
 hold off
 export_fig('SelfHealingHermite3D','-png','-transparent')
+
+
+%%
+
+distances = [1,3,inf];
+
+texts     = {'1/5','1/10','0'};
+
+
+close(figure(800))
+fig800 = figure(800);
+
+kk = 1;
+for jj = distances
+  
+  index = floor(Nz/jj);
+  if index == 0
+    index = 1;
+  end
+  
+
+  gg1 = Wo(:,index,:);
+  gg1 = reshape(gg1,[Nx,Nx]);
+  figure(1000)
+  plotOpticalField(x,x,abs(gg1).^2,mapgreen,'','');
+  axis square
+  axis off
+  plotRaysSquare(rayH12(index),'m',1,1);
+  plotRaysSquare(rayH21(index),'y',1,1);
+  plotRaysSquare(rayH11(index),'r',1,1);
+  plotRaysSquare(rayH22(index),'c',1,1);
+%   title(['$z$ = ', texts{kk}],'Interpreter','latex')
+axis off
+  kk = kk+1 ;
+
+  export_fig(['HermitePropagation',num2str(jj)],'-png','-transparent')
+
+  
+end
