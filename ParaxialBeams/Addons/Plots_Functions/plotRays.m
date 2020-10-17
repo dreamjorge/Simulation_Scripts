240927228ee1d0358588e592573e1c6fe2b9087b
jorge ugalde <jorge-kun@live.com>
2020-10-16 19:11:01 -0500

    removed files

--- ParaxialBeams/Addons/Plots_Functions/plotRays.m
+++
@@ -1,9 +0,0 @@
-function [h] = plotRays(rayObject,color,scaleX,scaleY)
-%% Function for plot rays in x,y plane
-  hold on
-
-  h = scatter(rayObject.xCoordinate*scaleX,rayObject.yCoordinate*scaleY,10,color,'filled','o');
-
-  hold off
-
-end
